ee019f98733a3d56b6a8f76d7dca8bbe2b8c9448
kaiserdc <koreshdc@gmail.com>
2025-01-28 13:07:07 +0200

    (feat): Выполненное домашнее задание 3

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import App from './App.tsx'
 import {Index} from "./pages";
 import {HomeWork1} from "./pages/home-1.tsx";
+import {UseHover} from "./pages/use-hover.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path={'/'} element={<App />}>
               <Route index element={<Index/>}/>
               <Route path={'use-fetch'} element={<HomeWork1/>}/>
+              <Route path={'use-hover'} element={<UseHover/>}/>
           </Route>
       </Routes>
 
